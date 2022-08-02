1f7201888cc62d61e882d90588b0dc54ff1b04fe
Kerem Çalık <105204556+keremcalikdev@users.noreply.github.com>
2022-08-02 13:16:24 +0300

    Update data.dart

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,21 +1,17 @@
+import 'package:notepad/utils/time.dart';
+
 import '../models/note.dart';
 
 List<Note>? noteData = [
-  Note(title: 'test' , text: 'bla bla bla.. bla bla' , editTime: ''),
-  Note(title: 'test' , text: 'bla bla bla.. bla bla', editTime: ''),
-  Note(title: 'test' , text: 'bla bla bla.. bla bla', editTime: ''),
-  Note(title: 'test' , text: 'bla bla bla.. bla bla', editTime: ''),
-  Note(title: 'test' , text: 'bla bla bla.. bla bla', editTime: ''),
-  Note(title: 'test' , text: 'bla bla bla.. bla bla', editTime: ''),
-  Note(title: 'test' , text: 'bla bla bla.. bla bla', editTime: ''),
-  Note(title: 'test' , text: 'bla bla bla.. bla bla', editTime: ''),
-  Note(title: 'test' , text: 'bla bla bla.. bla bla', editTime: ''),
-  Note(title: 'test' , text: 'bla bla bla.. bla bla', editTime: ''),
-  Note(title: 'test' , text: 'bla bla bla.. bla bla', editTime: ''),
+  Note(
+      title: 'London',
+      text:
+          'London, the capital of England and the United Kingdom, is a 21st-century city with history stretching back to Roman times. At its centre stand the imposing Houses of Parliament, the iconic ‘Big Ben’ clock tower and Westminster Abbey, site of British monarch coronations. Across the Thames River, the London Eye observation wheel provides panoramic views of the South Bank cultural complex, and the entire city. ',
+      editTime: todayDate),
+  Note(
+      text:
+          'Crocodiles (family Crocodylidae) or true crocodiles are large semiaquatic reptiles that live throughout the tropics in Africa, Asia, the Americas and Australia. The term crocodile is sometimes used even more loosely to include all extant members of the order Crocodilia, which includes the alligators and caimans (family Alligatoridae), the gharial and false gharial (family Gavialidae), and all other living and fossil Crocodylomorpha. Although they appear similar, crocodiles, alligators and the gharial belong to separate biological families. The gharial, with its narrow snout, is easier to distinguish, while morphological differences are more difficult to spot in crocodiles and alligators. The most obvious external differences are visible in the head, with crocodiles having narrower and longer heads, with a more V-shaped than a U-shaped snout compared to alligators and caimans. Another obvious trait is that the upper and lower jaws of the crocodiles are the same width, and the teeth in the lower jaw fall along the edge or outside the upper jaw when the mouth is closed; therefore, all teeth are visible, unlike an alligator, which possesses in the upper jaw small depressions into which the lower teeth fit. Also, when the crocodile\'s mouth is closed, the large fourth tooth in the lower jaw fits into a constriction in the upper jaw. For hard-to-distinguish specimens, the protruding tooth is the most reliable feature to define the species\' family.[1] Crocodiles have more webbing on the toes of the hind feet and can better tolerate saltwater due to specialized salt glands for filtering out salt, which are present, but non-functioning, in alligators. Another trait that separates crocodiles from other crocodilians is their much higher levels of aggression.[2]',
+      editTime: todayDate),
 ];
 
-List<String>? labelsData = [
-  'Travel',
-  'Work',
-  'Personel'
-];
+List<String>? labelsData = ['Travel', 'Work', 'Personel'];
